e840b8cb2b21437281ee942284859651f6c98465
akk0ga <45255672+akk0ga@users.noreply.github.com>
2021-03-03 18:48:42 +0100

    typing Mouse class

--- mouse/Mouse.py
+++ mouse/Mouse.py
@@ -8,6 +8,7 @@ from mouse.Voice import Voice
 from mouse.Action import Action
 from random import randint
 
+
 class Mouse(Listener, Action, Voice):
 
     def __init__(self, x: int = 960, y: int = 540, voice_mode: str = 'en'):
@@ -25,66 +26,66 @@ class Mouse(Listener, Action, Voice):
 
         # attribute
         self.__controller = mouse.Controller()
-        self.__x = x
-        self.__y = y
-        self.__voice = Voice(voice_mode)
-        self.__action = Action()
+        self.__x: int = x
+        self.__y: int = y
+        self.__voice: Voice = Voice(voice_mode)
+        self.__action: Action = Action()
 
         # for the log
-        self.log = 'log/log.txt'
+        self.log: str = 'log/log.txt'
 
-    def get_axes(self):
+    def get_axes(self) -> tuple:
         """
         get the __mouse axes
         :rtype: tuple
         """
-        original_axes = self.__controller.position
+        original_axes: tuple = self.__controller.position
         return original_axes
 
-    def set_position(self):
+    def set_position(self) -> None:
         """
         set the __mouse position to another axes
-        :rtype: void
+        :rtype: None
         """
         self.__controller.position = (self.__x, self.__y)
 
-    def __on_move(self, x, y):
+    def __on_move(self, x, y) -> None:
         """for __mouse movement"""
-        self.__x = x
-        self.__y = y
+        self.__x: int = x
+        self.__y: int = y
         print('=======================================')
         print(f'new position\n\tX: {x}\n\tY: {y}')
         print('=======================================\n')
 
-    def __axes_difference(self, original_axes: tuple, new_axes: tuple):
+    def __axes_difference(self, original_axes: tuple, new_axes: tuple) -> int:
         # split tuple
         original_x, original_y = original_axes
         new_x, new_y = new_axes
 
         # make the difference between axes x atm
-        result = original_x - new_x
+        result: int = original_x - new_x
         return abs(result)
 
-    def launch(self):
+    def launch(self) -> None:
         self.__voice._launch()
         time.sleep(0.3)
         self.__voice._start()
         self.set_position()
 
-    def __confused(self):
+    def __confused(self) -> None:
         self.set_position()
         self.__voice._confused()
 
-    def listener_event_mouse(self):
+    def listener_event_mouse(self) -> None:
         # listen __mouse event
         with mouse.Events() as event:
-            original_axes = self.get_axes()
+            original_axes: tuple = self.get_axes()
             time.sleep(0.3)
 
         # if __mouse move
         if event and not mixer.music.get_busy():
-            new_axes = self.get_axes()
-            difference = self.__axes_difference(new_axes, original_axes)
+            new_axes: tuple = self.get_axes()
+            difference: int = self.__axes_difference(new_axes, original_axes)
 
             # check choice
             if difference > 1000:
@@ -105,16 +106,17 @@ class Mouse(Listener, Action, Voice):
     """
     attribute parameter
     """
-    def set_x(self, x: int):
+
+    def set_x(self, x: int) -> None:
         self.__x = x
 
-    def set_y(self, y: int):
+    def set_y(self, y: int) -> None:
         self.__y = y
 
-    def get_x(self):
+    def get_x(self) -> int:
         return self.__x
 
-    def get_y(self):
+    def get_y(self) -> int:
         return self.__y
 
     x = property(get_x, set_x)
